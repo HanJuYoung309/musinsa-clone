ba91aa29cdeccf178d3a70f79f802b1430966d81
HanJuYoung309 <juyg407@naver.com>
2025-11-03 00:38:26 +0900

    DB YML 파일 오류 해결

--- src/main/java/com/demo/musinsaclone/MusinsaCloneApplication.java
+++ src/main/java/com/demo/musinsaclone/MusinsaCloneApplication.java
@@ -3,9 +3,11 @@ package com.demo.musinsaclone;
 import com.demo.musinsaclone.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.demo.musinsaclone.domain")
 @EnableConfigurationProperties(JwtProperties.class)
 public class MusinsaCloneApplication {
 
